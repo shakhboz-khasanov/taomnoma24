4fb4b778eb7275a160e0741685d8ce575a676512
shakhboz <devacc963@gmail.com>
2024-01-18 15:13:47 +0500

    initial static files setup

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django. conf import settings
+from django.conf.urls.static import static
 
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -29,3 +31,9 @@ urlpatterns = [
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
